8c725979543d820606840bdcba692abf29bddda2
Stefan Irrgang <122272798+StefanIrrgang@users.noreply.github.com>
2023-09-18 11:03:35 +0200

    added menu

---
+++ stefanirrgang/src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss']
+})
+export class SlideshowComponent implements OnInit {
+  images = ['code.jpg', 'computer.jpg', 'university.jpg'];
+  headlines = ['Born to code', 'Bring engineering to the next level', 'Graduated at University']
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 2);
+    }, 8000);
+
+  }
+
+}
